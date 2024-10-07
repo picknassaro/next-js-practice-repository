0724e3d09bd7876336eef0a7570bc82e59ca6fdc
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-07 11:52:53 -0400

    Learned basic caching

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,7 +5,9 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 }
+  });
   const users: User[] = await response.json();
 
   return (
