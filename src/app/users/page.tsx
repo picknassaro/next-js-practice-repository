84b704af3659863816c14c88e54afd2d63eebff5
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-07 11:50:03 -0400

    Learned basic data fetching and presentation

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,23 @@
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
