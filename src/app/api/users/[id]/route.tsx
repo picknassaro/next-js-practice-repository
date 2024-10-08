36570e651d08daf0a7059f0d95cc6a26a03247df
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-08 11:41:20 -0400

    Took what was learned in the last commit used it to figure out PUT and DELETE requests

--- src/app/api/users/[id]/route.tsx
+++ src/app/api/users/[id]/route.tsx
@@ -4,6 +4,32 @@ export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  if (params.id > 10) return NextResponse.json({ error: "Not found" }, { status: 404 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
   return NextResponse.json({ id: params.id, name: "User " + params.id });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: params.id, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ message: "User deleted successfully" }, { status: 200 });
 }
