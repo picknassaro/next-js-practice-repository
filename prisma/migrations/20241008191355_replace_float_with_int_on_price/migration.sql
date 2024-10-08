9a8804acd24ae69136e7379ce48f7b685b2c6dd1
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-08 15:18:33 -0400

    Figured out on my own how to get all products and add a new product

---
+++ prisma/migrations/20241008191355_replace_float_with_int_on_price/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Float` to `Int`.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "stock" INTEGER NOT NULL
+);
+INSERT INTO "new_Product" ("id", "name", "price", "stock") SELECT "id", "name", "price", "stock" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
--- src/app/api/products/route.tsx
+++ src/app/api/products/route.tsx
@@ -1,52 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../../../prisma/client";
 
-const sampleData = [
-  { id: 1, name: "Milk", price: 2.5, stock: 50 },
-  { id: 2, name: "Eggs", price: 3.0, stock: 100 },
-  { id: 3, name: "Bread", price: 4.0, stock: 60 },
-];
-
-export function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const name = searchParams.get("name");
-  const price = searchParams.get("price");
-  const stock = searchParams.get("stock");
-
-  let filteredData = sampleData;
-
-  if (id) {
-    filteredData = filteredData.filter((item) => item.id === parseInt(id));
-  }
-  if (name) {
-    filteredData = filteredData.filter((item) => item.name.toLowerCase().includes(name.toLowerCase()));
-  }
-  if (price) {
-    filteredData = filteredData.filter((item) => item.price === parseFloat(price));
-  }
-  if (stock) {
-    filteredData = filteredData.filter((item) => item.stock === parseInt(stock));
-  }
-
-  if (filteredData.length === 0) {
-    return NextResponse.json({ error: "Not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(filteredData);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
 
-  if (!validation.success) {
+  if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  }
 
-  const newId = sampleData.length + 1;
-  const newProduct = { id: newId, ...body };
-  sampleData.push(newProduct);
+  const productCheck = await prisma.product.findUnique({ where: { name: body.name } });
+  if (productCheck)
+    return NextResponse.json({ error: "Product already exists" }, { status: 400 });
 
+  const newProduct = await prisma.product.create({
+    data: { name: body.name, price: parseInt(body.price), stock: parseInt(body.stock) },
+  });
   return NextResponse.json(newProduct, { status: 201 });
 }
