c7ac1bb922c81ce63f9bd3d945500e246223beaf
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-08 14:43:36 -0400

    Learned how to make sure user being added doesn't already exist

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -15,6 +15,12 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const newUser = await prisma.user.create({ data: { name: body.name, email: body.email } });
+  const userCheck = await prisma.user.findUnique({ where: { email: body.email } });
+  if (userCheck)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: { name: body.name, email: body.email },
+  });
   return NextResponse.json(newUser, { status: 201 });
 }
