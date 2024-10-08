3e0ac88df5272ffdd313aa0169b6f63d0253da61
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-08 11:40:45 -0400

    Learned about POST requests

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "Alice" },
     { id: 2, name: "Bob" },
     { id: 3, name: "Charlie" },
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
