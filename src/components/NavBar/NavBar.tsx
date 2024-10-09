a5ab6ba32015a3090d4d66fb596a1538968859bf
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-09 14:26:13 -0400

    Used client-side session access to update the nav bar to show the logged in user. Also put a sign out placeholder that will become a link or button later.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,38 @@
-import Link from "next/link"
+"use client";
+
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <nav className="flex bg-slate-500 text-white p-5">
-      <Link href="/" className="mr-5">Home</Link>
-      <Link href="/users" className="mr-5">Users</Link>
-      <Link href="/admin" className="mr-5">Admin</Link>
-      <Link href="/api/auth/signin" className="mr-5">Login</Link>
+      <Link href="/" className="mr-5">
+        Home
+      </Link>
+      <Link href="/users" className="mr-5">
+        Users
+      </Link>
+      <Link href="/admin" className="mr-5">
+        Admin
+      </Link>
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
+      {status === "loading" && <p>Loading...</p>}
+      {status === "authenticated" && (
+        <>
+          <div className="mr-5">
+            {session?.user?.name || session?.user?.email || "Unidentified User"}
+          </div>
+          <div className="mr-5">Sign out</div>
+        </>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
