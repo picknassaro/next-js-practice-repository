205fea5d50bc458a9c47baaadeabe5efeada5a10
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-09 14:49:59 -0400

    Added Prisma stuff to database in place of old user table. Fixed nextauth route to use JSON web token as authentication strategy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../../../prisma/client";
 
-export const options = {
+export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
   secret: process.env.SECRET!,
 };
 
